251f152fd62a47cd05f874381479d33d1a20b1de
Mednasa <girgin183@gmail.com>
2024-03-10 11:03:32 +0300

    The product was successfully added to the cart.Gift card and discount coupon transactions were successfully made.proceeded to the checkout process.

--- src/US_208/usingCouponAndGiftCards.java
+++ src/US_208/usingCouponAndGiftCards.java
@@ -32,6 +32,52 @@ public class usingCouponAndGiftCards extends BaseDriver {
 
         WebElement loginButton = driver.findElement(By.xpath("(//input[@type='submit'])[2]"));
         actionsDriver.moveToElement(loginButton).click().build().perform();
+
+        WebElement computers = driver.findElement(By.cssSelector("[class='inactive']:nth-child(2)>a"));
+        actionsDriver.moveToElement(computers).click().build().perform();
+
+        WebElement noteBooks = driver.findElement(By.xpath("(//div[@class='sub-category-item'])[2]//a"));
+        actionsDriver.moveToElement(noteBooks).click().build().perform();
+
+        WebElement addtoCart = driver.findElement(By.xpath("//input[@value='Add to cart']"));
+        actionsDriver.moveToElement(addtoCart).click().build().perform();
+
+        WebElement shoppingCart = driver.findElement(By.xpath("(//span[@class='cart-label'])[1]"));
+        actionsDriver.moveToElement(shoppingCart).click().build().perform();
+
+        WebElement emptyValidation = driver.findElement(By.xpath("(//*[@class='product-name'])"));
+        Assert.assertTrue(emptyValidation.getText().contains("Laptop"), "Your Shopping Cart is empty");
+
+        WebElement discountCode = driver.findElement(By.xpath("//input[@name='applydiscountcouponcode']"));
+        actionsDriver.moveToElement(discountCode).click().build().perform();
+
+        WebElement discountWarning = driver.findElement(By.xpath("//div[@class='message']"));
+        wait.until(ExpectedConditions.visibilityOf(discountWarning));
+
+        Assert.assertTrue(discountWarning.isDisplayed(), "Coupon code correct");
+
+        WebElement giftCards = driver.findElement(By.xpath("//input[@name='applygiftcardcouponcode']"));
+        actionsDriver.moveToElement(giftCards).click().build().perform();
+
+        WebElement giftWarning = driver.findElement(By.xpath("//div[@class='message']"));
+        wait.until(ExpectedConditions.visibilityOf(giftWarning));
+
+        Assert.assertTrue(giftWarning.isDisplayed(), "Gift cards code correct");
+
+        WebElement country = driver.findElement(By.xpath("//*[@id='CountryId']"));
+        Select ddMenuCountry = new Select(country);
+        ddMenuCountry.selectByVisibleText("United States");
+
+        WebElement province = driver.findElement(By.xpath("//*[@id='StateProvinceId']"));
+        Select ddMenuProvince = new Select(province);
+        ddMenuProvince.selectByVisibleText("Oklahoma");
+
+        WebElement termOfService = driver.findElement(By.xpath("//*[@id='termsofservice']"));
+        actionsDriver.moveToElement(termOfService).click().build().perform();
+
+        WebElement checkOut = driver.findElement(By.xpath("//*[@id='checkout']"));
+        actionsDriver.moveToElement(checkOut).click().build().perform();
+
         waitAndClose();
     }
 }
